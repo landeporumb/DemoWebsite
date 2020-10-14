cf258da6e10577809e0f5bb78dd8c43c6a34737a
unknown <landeporumb@gmail.com>
2020-10-14 15:02:43 +0300

    testing the commit

--- resources/views/layouts/header/nav.blade.php
+++ resources/views/layouts/header/nav.blade.php
@@ -2,8 +2,7 @@
 <header id="header">
     <a class="logo" href="/">Home</a>
     <nav>
-        <a href="elements.html">Elements</a>
-        <a href="generic.html">Generic</a>
+        <a href="promotions">Promotions</a>
         <a href="logout">Log out</a>
     </nav>
 </header>
